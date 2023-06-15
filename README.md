a74f3ce7f5044df4038ed11ebf9ab27c5f6aa98c
camitak <camila.takemotob@gmail.com>
2023-06-15 14:43:41 +0100

    v2

---
+++ README.md
@@ -0,0 +1,3 @@
+# MemoryGame
+
+Developed with Unreal Engine 5
---
+++ Source/MemoryGame/MemoryGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MemoryGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEMORYGAME_API AMemoryGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
